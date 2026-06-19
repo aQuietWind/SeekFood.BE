b86a3a7dd7685d8065796ea002896d3f4e9c101b
定风久久 <750635126@qq.com>
2026-06-19 09:42:19 +0800

    项目pom初始化完成

---
+++ Admin/src/test/java/com/seek/food/food/admin/AdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seek.food.food.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ Rider/src/main/java/com/seek/food/rider/RiderApplication.java
@@ -0,0 +1,13 @@
+package com.seek.food.rider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RiderApplication.class, args);
+    }
+
+}
