0a9ce16f3c138cdfc0f76dbd55e824637266e1ea
定风久久 <750635126@qq.com>
2026-07-15 22:40:10 +0800

    springDataEs重构版es同步代码块修复Bug完成

--- Merchant/src/main/java/com/seek/food/merchant/Consumer/EsSyncMerchantConsumer.java
+++ Merchant/src/main/java/com/seek/food/merchant/Consumer/EsSyncMerchantConsumer.java
@@ -1,6 +1,5 @@
 package com.seek.food.merchant.Consumer;
 
-import co.elastic.clients.elasticsearch.ElasticsearchClient;
 import com.seek.food.config.Enum.MQNameKeyEnum;
 import com.seek.food.config.NacosConfig.Common.CommonParamRulesConfig;
 import com.seek.food.config.NacosConfig.Merchant.MerchantEsTableConfig;
@@ -8,8 +7,6 @@ import com.seek.food.config.NacosConfig.Merchant.MerchantRedisKeyConfig;
 import com.seek.food.dto.Merchant.MerchantEsDTO;
 import com.seek.food.merchant.EsRepository.MerchantRepository;
 import com.seek.food.merchant.Mapper.MerchantMapper;
-import com.seek.food.util.Exception.BizException;
-import com.seek.food.util.Exception.ErrorCodeEnum;
 import com.seek.food.util.Redis.RedisUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
@@ -17,11 +14,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
 import org.springframework.data.elasticsearch.core.convert.ElasticsearchConverter;
 import org.springframework.data.elasticsearch.core.document.Document;
+import org.springframework.data.elasticsearch.core.mapping.IndexCoordinates;
 import org.springframework.data.elasticsearch.core.query.UpdateQuery;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
 
-import java.util.Map;
 
 
 @Component
@@ -33,17 +30,21 @@ public class EsSyncMerchantConsumer {
     private final CommonParamRulesConfig commonParamRulesConfig;
     private final ElasticsearchOperations elasticsearchOperations;
     private final ElasticsearchConverter elasticsearchConverter;
+    private final MerchantEsTableConfig merchantEsTableConfig;
+    private final MerchantRepository merchantRepository;
 
     @Autowired
     public EsSyncMerchantConsumer(StringRedisTemplate stringRedisTemplate
     , MerchantMapper merchantMapper, MerchantRedisKeyConfig merchantRedisKeyConfig, CommonParamRulesConfig commonParamRulesConfig
-    , ElasticsearchOperations elasticsearchOperations, ElasticsearchConverter elasticsearchConverter) {
+    , ElasticsearchOperations elasticsearchOperations, ElasticsearchConverter elasticsearchConverter, MerchantEsTableConfig merchantEsTableConfig, MerchantRepository merchantRepository) {
         this.stringRedisTemplate = stringRedisTemplate;
         this.merchantMapper = merchantMapper;
         this.merchantRedisKeyConfig = merchantRedisKeyConfig;
         this.commonParamRulesConfig = commonParamRulesConfig;
         this.elasticsearchOperations = elasticsearchOperations;
         this.elasticsearchConverter = elasticsearchConverter;
+        this.merchantEsTableConfig = merchantEsTableConfig;
+        this.merchantRepository = merchantRepository;
     }
 
 
@@ -59,13 +60,10 @@ public class EsSyncMerchantConsumer {
         if (merchant.getMerchantLocation().equals(",")) merchant.setMerchantLocation(null);
         System.err.println(merchant.getMerchantLocation());
         //尝试同步
-        try {
-            updateOnly(merchant);
-        }catch (Exception e){throw new BizException(ErrorCodeEnum.SERVER_ERROR);}
-        finally {
-            //无论失败成功都更改状态，防止出现永远不更新的问题
-            ackState(merchantId);
-        }
+        updateOnly(merchant);
+        //无论失败成功都更改状态，防止出现永远不更新的问题
+        ackState(merchantId);
+        merchantRepository.save(merchant);
     }
 
     //快速修改状态
@@ -75,16 +73,8 @@ public class EsSyncMerchantConsumer {
 
 
     public void updateOnly(MerchantEsDTO merchant) {
-        // DTO转Map，自动识别@Field、全局下划线策略
-        Document sourceMap = elasticsearchConverter.mapObject(merchant);
-        // 构建更新对象，全量覆盖_source
-        UpdateQuery updateQuery = UpdateQuery.builder(String.valueOf(merchant.getMerchantId()))
-                .withDocument(sourceMap)
-                // 核心配置：不存在时不创建文档
-                .withDocAsUpsert(false)
-                .build();
         // 执行更新，无文档则无任何写入
-        elasticsearchOperations.update(updateQuery);
+        elasticsearchOperations.update(merchant, IndexCoordinates.of(merchantEsTableConfig.getIndexName()));
     }
 
 
