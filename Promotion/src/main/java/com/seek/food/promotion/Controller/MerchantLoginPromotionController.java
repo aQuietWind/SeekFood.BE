620a2d41eac7b8d2ff059aceecf3355e1652a6e9
定风久久 <750635126@qq.com>
2026-07-23 12:16:10 +0800

    完成了Promotion模块的Service层设计

--- Promotion/src/main/java/com/seek/food/promotion/Controller/MerchantLoginPromotionController.java
+++ Promotion/src/main/java/com/seek/food/promotion/Controller/MerchantLoginPromotionController.java
@@ -1,10 +1,19 @@
 package com.seek.food.promotion.Controller;
 
+import com.seek.food.dto.Common.Result;
+import com.seek.food.dto.Promotion.MerchantLoginPromotionDTO;
 import com.seek.food.promotion.Enum.RequestPathEnum;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping(RequestPathEnum.Merchant_Login_Promotion)
 @RestController
 public class MerchantLoginPromotionController {
+
+    @PostMapping
+    public Result<MerchantLoginPromotionDTO> insertPromotion(MerchantLoginPromotionDTO merchantLoginPromotionDTO) {
+
+        return Result.success();
+    }
 }
