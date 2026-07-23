199eecdc757b47d94316e1b13305039f95baa21c
定风久久 <750635126@qq.com>
2026-07-23 12:35:53 +0800

    完成了Promotion模块的登录活动Service层实现类的查询和插入功能

--- Promotion/src/main/java/com/seek/food/promotion/Service/Impl/MerchantLoginPromotionServiceImpl.java
+++ Promotion/src/main/java/com/seek/food/promotion/Service/Impl/MerchantLoginPromotionServiceImpl.java
@@ -1,9 +1,19 @@
 package com.seek.food.promotion.Service.Impl;
 
+import com.seek.food.config.Data.RedisKeyData;
+import com.seek.food.config.NacosConfig.Common.CommonParamRulesConfig;
+import com.seek.food.config.NacosConfig.Promotion.PromotionParamsRulesConfig;
+import com.seek.food.config.NacosConfig.Promotion.PromotionRedisKeyConfig;
 import com.seek.food.dto.Promotion.MerchantLoginPromotionDTO;
+import com.seek.food.promotion.Caffeine.MerchantLoginPromotionCaffeine;
+import com.seek.food.promotion.Mapper.MerchantLoginPromotionMapper;
 import com.seek.food.promotion.Service.MerchantLoginPromotionService;
+import com.seek.food.util.CommonUtil.IdUtil;
+import com.seek.food.util.Context.TokenIdContext;
+import com.seek.food.util.Redis.RedisUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,33 +23,100 @@ import java.util.List;
 @Slf4j
 public class MerchantLoginPromotionServiceImpl implements MerchantLoginPromotionService {
 
+    private final CommonParamRulesConfig commonParamRulesConfig;
+    private final PromotionParamsRulesConfig promotionParamsRulesConfig;
+    private final StringRedisTemplate stringRedisTemplate;
+    private final PromotionRedisKeyConfig promotionRedisKeyConfig;
+    private final MerchantLoginPromotionMapper merchantLoginPromotionMapper;
+    private final MerchantLoginPromotionCaffeine merchantLoginPromotionCaffeine;
+
+    public MerchantLoginPromotionServiceImpl(CommonParamRulesConfig commonParamRulesConfig, PromotionParamsRulesConfig promotionParamsRulesConfig
+            , StringRedisTemplate stringRedisTemplate, PromotionRedisKeyConfig promotionRedisKeyConfig
+            , MerchantLoginPromotionMapper merchantLoginPromotionMapper, MerchantLoginPromotionCaffeine merchantLoginPromotionCaffeine) {
+        this.commonParamRulesConfig = commonParamRulesConfig;
+        this.promotionParamsRulesConfig = promotionParamsRulesConfig;
+        this.stringRedisTemplate = stringRedisTemplate;
+        this.promotionRedisKeyConfig = promotionRedisKeyConfig;
+        this.merchantLoginPromotionMapper = merchantLoginPromotionMapper;
+        //初始化id计数器，如果觉得写这里不符合工业化方针，可以写到@Init里
+        stringRedisTemplate.opsForValue().setIfAbsent(promotionRedisKeyConfig.getMerchantLoginPromotionIdCount().getName(),""+commonParamRulesConfig.getIdCapacity());
+        this.merchantLoginPromotionCaffeine = merchantLoginPromotionCaffeine;
+    }
+
     //新增活动
     @Override
-    public void insertPromotion(MerchantLoginPromotionDTO merchantLoginPromotionDTO){
-        
+    public void insertPromotion(MerchantLoginPromotionDTO promotion){
+        //依旧检查参数格式，如果觉得这里代码繁琐，可以封装到实体类或者配置类的方法中
+        promotionParamsRulesConfig.promotionTitleCheck(promotion.getPromotionTitle());
+        promotionParamsRulesConfig.promotionDescriptionCheck(promotion.getPromotionDescription());
+        promotionParamsRulesConfig.promotionNoticeCheck(promotion.getPromotionNotice());
+        promotionParamsRulesConfig.durationDayCheck(promotion.getStartTime(), promotion.getEndTime());
+        //获取商家id
+        long merchantId=quickGetMerchantId();
+        //检查冷却
+        quickCooldown(promotionRedisKeyConfig.getMerchantLoginPromotionInsertCooldown(),merchantId);
+        //放入必要数据
+        promotion.setMerchantId(merchantId);
+        promotion.setPromotionId(IdUtil.IdGenerateByIncrease(promotionRedisKeyConfig.getMerchantLoginPromotionIdCount().getName(),stringRedisTemplate));
+        //插入数据
+        merchantLoginPromotionMapper.insertPromotion(promotion);
     }
 
     //获取某商家的简易活动介绍
     @Override
     public List<MerchantLoginPromotionDTO> getSimple(int start, int need, long merchantId){
-
+        //检查参数格式
+        commonParamRulesConfig.needNumberCheck(need);
+        commonParamRulesConfig.merchantIdCheck(merchantId);
+        //检查冷却，防止脚本频繁从DB中获取数据,因为正常情况下前端恰好可以通过节流等操作控制间隔时间，所以1~2秒冷却对于正常用户使用是不会有任何影响的
+        quickCooldown(promotionRedisKeyConfig.getMerchantLoginPromotionGetSimpleCooldown(),quickGetStringId());
+        //返回数据
+        return merchantLoginPromotionMapper.getSimple(start, need, merchantId);
     }
 
     //获取某商家还在有效期的简易活动介绍
     @Override
     public List<MerchantLoginPromotionDTO> getSimpleEffective(int start, int need,long merchantId){
-
+        //检查参数格式
+        commonParamRulesConfig.needNumberCheck(need);
+        commonParamRulesConfig.merchantIdCheck(merchantId);
+        //检查冷却，防止脚本频繁从DB中获取数据,因为正常情况下前端恰好可以通过节流等操作控制间隔时间，所以1~2秒冷却对于正常用户使用是不会有任何影响的
+        quickCooldown(promotionRedisKeyConfig.getMerchantLoginPromotionGetSimpleEffectiveCooldown(),quickGetStringId());
+        //返回数据
+        return merchantLoginPromotionMapper.getSimple(start, need, merchantId);
     }
 
     //获取详细的活动信息
     @Override
     public MerchantLoginPromotionDTO getDetail(long promotionId){
-
+        //检查参数格式
+        commonParamRulesConfig.commonIdCheck(promotionId);
+        return merchantLoginPromotionCaffeine.getAndAutoLoad(promotionId
+                ,stringRedisTemplate
+                ,promotionRedisKeyConfig.getMerchantLoginPromotionMessageCaffeine().getRedisKey(promotionId)
+                ,promotionRedisKeyConfig.getMerchantLoginPromotionMessageCaffeine().getDuration()
+                ,MerchantLoginPromotionDTO.class,k->merchantLoginPromotionMapper.getDetail(promotionId));
     }
 
     //通过活动获取优惠券
     @Override
     public void getVoucher(long promotionId){
 
+    }
+
+    private void quickCooldown(RedisKeyData key,Object id){
+        RedisUtil.checkCooldown(stringRedisTemplate,key.getRedisKey(id),key.getDuration());
+    }
+
+    private long quickGetMerchantId(){
+        return TokenIdContext.getAndCheck(commonParamRulesConfig.getMerchantIdStart(),commonParamRulesConfig.getIdCapacity());
+    }
+
+    private long quickGetUserId(){
+        return TokenIdContext.getAndCheck(commonParamRulesConfig.getUserIdStart(),commonParamRulesConfig.getIdCapacity());
+    }
+
+    private String quickGetStringId(){
+        return TokenIdContext.get();
     }
 }
