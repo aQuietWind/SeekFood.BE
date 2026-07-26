a35d6118d58dea8703a453d139e340c75bd205d4
定风久久 <750635126@qq.com>
2026-07-26 12:02:17 +0800

    完成了Rider模块的Rider区块的Controller层代码

--- Rider/src/main/java/com/seek/food/rider/Service/RiderService.java
+++ Rider/src/main/java/com/seek/food/rider/Service/RiderService.java
@@ -1,13 +1,14 @@
 package com.seek.food.rider.Service;
 
+import com.seek.food.dto.Rider.RiderDTO;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface RiderService {
+    public RiderDTO getDetail(long riderId);
+    public RiderDTO getSelf();
     public void updatePersonImage(MultipartFile file);
-    public void updatePassword(String phoneNumber, String password, String opt);
-    public void getDetail(long riderId);
-    public void getSelf();
+    public String getUpdatePasswordOpt();
+    public void updatePassword(String password, String opt);
     public String getDeleteOpt();
     public void delete(String opt);
-    public String getUpdatePasswordOpt();
 }
