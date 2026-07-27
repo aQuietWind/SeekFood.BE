2a923b07398b69da52429ac90e186630fa5caa46
定风久久 <750635126@qq.com>
2026-07-27 10:13:37 +0800

    完成Comment模块的实体类设计

---
+++ Comment/src/main/java/com/seek/food/food/comment/Caffeine/FirstCommentCaffeine.java
@@ -0,0 +1,39 @@
+package com.seek.food.food.comment.Caffeine;
+
+import com.github.benmanes.caffeine.cache.Caffeine;
+import com.seek.food.config.NacosConfig.Employee.EmployeeCaffeineConfig;
+import com.seek.food.dto.Employee.EmployeeDTO;
+import com.seek.food.util.Caffeine.JvmCaffeineParent;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
+
+@Component
+public class FirstCommentCaffeine extends JvmCaffeineParent<Long, EmployeeDTO> {
+    // 构造注入配置
+    private final EmployeeCaffeineConfig employeeCaffeineConfig;
+    @Autowired
+    public FirstCommentCaffeine(EmployeeCaffeineConfig employeeCaffeineConfig) {
+        this.employeeCaffeineConfig = employeeCaffeineConfig;
+    }
+
+    // 容器启动构建缓存
+    @PostConstruct
+    public void init() {
+        super.CACHE = Caffeine.newBuilder()
+                .maximumSize(employeeCaffeineConfig.getEmployee().getMaxSize())
+                .expireAfterWrite(employeeCaffeineConfig.getEmployee().getExpireTime(), TimeUnit.MINUTES)
+                .recordStats()
+                .build();
+    }
+
+    // 容器销毁清理缓存
+    @PreDestroy
+    public void destroy() {
+        super.CACHE.cleanUp();
+        super.CACHE.invalidateAll();
+    }
+}
---
+++ DTO/src/main/java/com/seek/food/dto/Comment/SecondCommentDTO.java
@@ -0,0 +1,22 @@
+package com.seek.food.dto.Comment;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
+public class SecondCommentDTO {
+    private Long secondCommentId;
+    private Long firstCommentId;
+    private Long userId;
+    private String commentImageAddr;
+    private String commentDescription;
+    private LocalDateTime createTime;
+    private Integer likeAmount;
+    private Boolean isMerchantComment;
+    private Boolean isDelete;
+}
