51571df6b134bd520a79114c221b3805e55f1bcc
定风久久 <750635126@qq.com>
2026-07-27 09:17:04 +0800

    完成剩余模块的完全初始化操作,可开始编码

--- Admin/src/main/java/com/seek/food/admin/AdminApplication.java
+++ Admin/src/main/java/com/seek/food/admin/AdminApplication.java
@@ -1,11 +1,19 @@
 package com.seek.food.admin;
 
+import com.seek.food.config.Import.AdminImport;
+import com.seek.food.config.Import.CommonImport;
+import com.seek.food.config.Import.MQImport;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableFeignClients         //开启Feign功能
+@MQImport
+@CommonImport
+@EnableScheduling
+@AdminImport
 public class AdminApplication {
 
     public static void main(String[] args) {
