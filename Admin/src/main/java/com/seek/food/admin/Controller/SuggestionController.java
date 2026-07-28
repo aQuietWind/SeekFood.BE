d4fbe1b09805968cccc5274f694b10dd9024db6f
定风久久 <750635126@qq.com>
2026-07-28 21:43:00 +0800

    完成Admin模块的建议三层架构全部代码编写

--- Admin/src/main/java/com/seek/food/admin/Controller/SuggestionController.java
+++ Admin/src/main/java/com/seek/food/admin/Controller/SuggestionController.java
@@ -1,10 +1,34 @@
 package com.seek.food.admin.Controller;
 
 import com.seek.food.admin.Enum.RequestPathEnum;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.seek.food.dto.Admin.SuggestionDTO;
+import com.seek.food.dto.Common.Result;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @RequestMapping(RequestPathEnum.Admin_Suggestion)
 @RestController
 public class SuggestionController {
+
+    //插入建议
+    @PostMapping
+    public Result<Void> insertSuggestion(String description, MultipartFile file){
+        return Result.success();
+    }
+
+    //批量查询建议
+    @GetMapping
+    public Result<List<SuggestionDTO>> getSuggestionList(int start, int number){
+        return Result.success();
+    }
+
+    //确认评阅建议
+    @PutMapping
+    public Result<Void> ackSuggestion(long suggestionId){
+        return Result.success();
+    }
 }
--- Admin/target/classes/MysqlCreate.sql
+++ Admin/target/classes/MysqlCreate.sql
@@ -7,5 +7,6 @@ CREATE TABLE `suggestion` (
                               `account_id` bigint NOT NULL COMMENT '发布该建议的账户id',
                               `suggestion_image_addr` varchar(50) UNIQUE COMMENT '该建议图片的地址',
                               `suggestion_description` varchar(500) COMMENT '建议内容',
-                              `create_time` datetime NOT NULL default now() COMMENT '创建时间'
+                              `create_time` datetime NOT NULL default now() COMMENT '创建时间',
+                              `is_ack` boolean NOT NULL default false COMMENT '是否确认查询'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='建议表';
